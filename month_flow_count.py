bad9b9e4747e4e696f2ebb34a1b343c38b0e4f39
ming <591177@qq.com>
2017-12-12 13:24:02 +0800

    fill0

--- month_flow_count.py
+++ month_flow_count.py
@@ -97,12 +97,35 @@ sheet.write(0, 2, '账号')
 sheet.write(0, 3, '平台')
 sheet.write(0, 4, '流量')
 sheet.write(0, 5, '日均流量')
-for i, r in enumerate(result):
-    sheet.write(i+1, 0, r[0].strftime(config.date_format))
-    sheet.write(i+1, 1, r[1])
-    sheet.write(i+1, 2, r[2])
-    sheet.write(i+1, 3, r[3])
-    sheet.write(i+1, 4, r[4])
+# for i, r in enumerate(result):
+#     sheet.write(i+1, 0, r[0].strftime(config.date_format))
+#     sheet.write(i+1, 1, r[1])
+#     sheet.write(i+1, 2, r[2])
+#     sheet.write(i+1, 3, r[3])
+#     sheet.write(i+1, 4, r[4])
+user_account_plat_dict = {}
+for r in result:
+    uap = r[1] + ':' + r[2] + ':' + r[3]
+    if uap in user_account_plat_dict:
+        user_account_plat_dict[uap][r[0].strftime(config.date_format)] = r[4]
+    else:
+        user_account_plat_dict[uap] = {r[0].strftime(config.date_format): r[4]}
+for i in range(30):
+    day = datetime.datetime.strptime('2017-11-1', config.date_format) + datetime.timedelta(i)
+    day_str = day.strftime(config.date_format)
+    for uap in user_account_plat_dict:
+        if day_str not in user_account_plat_dict[uap]:
+            user_account_plat_dict[uap][day_str] = 0
+
+for uap in user_account_plat_dict:
+    for day in user_account_plat_dict[uap]:
+        u, a, p = uap.split(':')
+        row = len(sheet.rows)
+        sheet.write(row, 0, day)
+        sheet.write(row, 1, u)
+        sheet.write(row, 2, a)
+        sheet.write(row, 3, p)
+        sheet.write(row, 4, user_account_plat_dict[uap][day])
 
 workbook.save('month_flow.xls')
 src_cur.close()
